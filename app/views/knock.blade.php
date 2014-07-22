28073416007e64326635b0738dc2f3b2535b4896
DoSomething <dosomething@DoSomethings-MacBook-Pro.local>
2014-07-22 10:08:44 -0400

    Save buttons pop up in each module after that module has been changed Moved delete button to the right Can modify module names and mdata Started work on adding new modules/campaigns Added JQuery dragging of modules Working on switching to angular dragging
    Included neue styles and proper fonts
    Added some new styling to modules

--- app/views/knock.blade.php
+++ app/views/knock.blade.php
@@ -13,17 +13,43 @@
 	var yesNoPaths = routing.yesNoPaths;
 
 
-
-	app = angular.module("codeModel", []);
+	app = angular.module("codeModel", ['ngAnimate']);
+	
+		app.directive("updateDb", function() {
+		var updateTemplate = '<button ng-click="update()" ng-show="$parent.edited" class="btn size">Update Database</button>';
+		return {
+			restrict : "A",
+			replace : true,
+			template : updateTemplate,
+			scope : {
+				value : "=updateDb"
+			},
+			controller : function($scope) {
+				$scope.view = {
+					changed : $scope.$parent
+				};
+				$scope.update = function() {
+					console.log($scope.$parent);
+					// console.log($scope);
+					saveFile(config);
+					$scope.$parent.edited = false;
+					
+					
+				}
+			}
+		} 	
+		
+	});	
 
 	app.directive("clickToEdit", function() {
 
-		var editorTemplate = '<span class="click-to-edit"><span class="octicon octicon-trashcan button fade" ng-click="remove(index)"></span>' + 
+		var editorTemplate = '<span class="click-to-edit">' +
 								'<span ng-hide="view.editorEnabled" ng-click="enableEditor()">' + '  {{value}} ' + '</span>' + 
 								'<span ng-show="view.editorEnabled">' + '<input ng-model="view.editableValue">' + 
-									'<span ng-click="save(index)" class="mega-octicon octicon-check med-icon button fade"></span>' + 
+									'<span ng-click="saveCode(index)" class="mega-octicon octicon-check med-icon button fade"></span>' + 
 									'<span ng-click="disableEditor()" class="mega-octicon octicon-remove-close med-icon button cancel fade"></span>' + 
 								'</span>' +
+								'<span class="octicon octicon-trashcan button fade right" ng-click="remove(index)"></span>'
 							'</span>';
 
 		return {
@@ -35,6 +61,8 @@
 				index : '@'
 			},
 			controller : function($scope) {
+				$scope.$parent.edited = false;
+				
 				$scope.view = {
 					editableValue : $scope.value,
 					editorEnabled : false
@@ -48,8 +76,10 @@
 				$scope.disableEditor = function() {//Close editor without saving changes
 					$scope.view.editorEnabled = false;
 				};
-
-				$scope.save = function(index) {//Save changes if input can be parsed as a number
+				
+				
+				
+				$scope.saveCode = function(index) {//Save changes if input can be parsed as a number
 					var val = parseInt($scope.view.editableValue);
 					if (isNaN(val)) {
 						alert('Code should be a number');
@@ -57,16 +87,21 @@
 					}
 					$scope.value = val;
 
+					$scope.disableEditor();
 
 					if ($scope.$parent.$parent.tip.optins) {			//I couldn't figure out a better way to access the collection that the scope is in
 																		//than to rise up 2 levels and manually drop into the appropriate collection
+																		
+						if($scope.$parent.$parent.tip.optins[index] === val) return; //Value didn't change so no need to show button 
 						$scope.$parent.$parent.tip.optins[index] = val;
 
 					} else {
+						if($scope.$parent.$parent.tip[index] === val) return; //Value didn't change so no need to show button
 						$scope.$parent.$parent.tip[index] = val;
 					}
 
-					$scope.disableEditor();
+					$scope.$parent.$parent.edited = true;
+					console.log($scope);
 				};
 				
 				
@@ -79,6 +114,8 @@
 					} else {
 						delete $scope.$parent.$parent.tip[index];
 					}
+					
+					$scope.$parent.$parent.edited = true;
 
 
 				};
@@ -112,31 +149,180 @@
 							return;
 						obj[keyname] = 0;
 					}
+					$scope.$parent.edited = true;
 
 				}
 			}
 		};
 	});
-
 	
+	app.directive("editName", function() {
+
+		var otherTemplate = '<span class="editName">' +
+								'<span ng-click="namePrompt()">' + '  {{value}} ' + '</span><br />' + 
+								'mData: <span ng-click="mdataPrompt()">{{ key }} </span>' +
+							'</span>';
+								
+								
+		return {
+			restrict : "A",
+			replace : true,
+			template : otherTemplate,
+			scope : {
+				value : "=editName",
+				key : "=key"
+				
+			},
+			controller : function($scope, $timeout) {
+				$scope.view = {
+					editableValue : $scope.value,
+					editableKey : $scope.key, 
+					editorEnabled : false
+				};
+
+				
+				$scope.namePrompt = function() {
+					var val = $scope.value;
+					var name = prompt('What would you like to rename this?', val);
+					if(!name) return;
+					$scope.value = name;
+					$scope.view.editableValue = name;
+
+				};
+				
+				$scope.mdataPrompt = function() {
+					// console.log($scope);
+					console.log($scope.$parent.$parent.campaignTips);
+					var val = $scope.key;
+					// console.log($scope.$parent);
+					var mdata = prompt('What would you like to change the mdata to?', val);
+					if(!mdata) return;
+					// $scope.$parent.$parent.campaignTips.replace(val, mdata);
+					$scope.$parent.$parent.campaignTips[mdata] = $scope.$parent.$parent.campaignTips[val];
+					delete $scope.$parent.$parent.campaignTips[val];
+					$scope.key = mdata;
+					$scope.view.editableKey = mdata;
+					$timeout(function() {
+						setDraggable();
+					}, 100);
+				};
+				
+				
+				
+			}
+		}	
+	});
+
+	app.directive("addNewModule", function() {
+		var addTemplate = '<button ng-click="add()">Hi</button>';
+		
+		return {
+			restrict : "A",
+			replace : true,
+			template : addTemplate,
+			scope : {
+				value : "=addNewModule"
+			},
+			controller : function($scope) {
+				$scope.add = function() {
+					var a = $scope.$parent.campaignTips;
+					
+					console.log(a);
+				}
+			}
+		} 	
+	});
+	
+	
+	app.directive('draggable', function () {
+		return {
+			restrict: 'A',
+			scope: { xpos: '=', ypos: '=' },
+			link: function (scope, element, attrs) {
+				element.draggable({
+					cursor: "move",
+					stop: function (event, ui) {
+						scope[attrs.xpos] = ui.position.left;
+	          			scope[attrs.ypos] = ui.position.top;
+	          			scope.$apply();
+	          			console.log(scope[attrs.xpos]);
+	          			console.log(scope);
+					}
+				});
+			}
+		};
+	});
 
 	app.controller("MainCtrl", function($scope) {
+		
+		
+		
 		$scope.startCampaignTransitions = startCampaignTransitions;
 		$scope.yesNoPaths = yesNoPaths;
 
 		$scope.campaignTips = campaignTips;
-
+		
+		$scope.sortId = function(campaign) {
+			var a = parseInt(campaign.sort);
+			console.log(a);
+			return a;
+		}
+		
 		$scope.notSorted = function(obj) {		//Display array in the order it's saved, not in Javascript's unhelpful default alphabetic sorting of numbers
 			if (!obj) {							
 				return [];
 			}
 			return Object.keys(obj);
 		}
+		
+		$scope.setPositions = function(){
+			
+			angular.forEach($scope.campaignTips, function(i,v) {
+				console.log(i);
+			});
+			
+			// console.log($scope.campaignTips);
+		}
+		
+		$scope.test = {
+			"a" : {
+				"name" : "Mal",
+				"sort" : 2
+			},
+			"b" : {
+				"name" : "Wash",
+				"sort" : 1
+			}
+		};
+		
 	});
+	
+	// app.filter('orderObjectBy', function() {
+	  // return function(items, field, reverse) {
+	    // var filtered = [];
+	    // angular.forEach(items, function(item) {
+	      // filtered.push(item);
+	    // });
+	    // filtered.sort(function (a, b) {
+	      // return (a[field] > b[field] ? 1 : -1);
+	    // });
+	    // if(reverse) filtered.reverse();
+	    // console.log("here");
+	    // return filtered;
+	  // };
+	// });
+	
+	app.filter('orderByUnicornHeight', function() {
+  return function(obj) {
+    
+    return [1,2,3];
+}});
+
 
 
 	angular.element(document).ready(function() {
 		angular.bootstrap('main', ["codeModel"]);
+		// setDraggable();
 	});
 
 
@@ -154,80 +340,62 @@
 			console.log(data);
 		});
 	}
+	
+	function setDraggable() {
+		$('.modules').draggable({
+			containment: 'parent',
+			stack : ".modules",
+			stop: function (event, ui) {
+			   scope[attrs.xpos] = ui.position.left;
+			   scope[attrs.ypos] = ui.position.top;
+			   scope.$apply();
+			}
+		});
+		
+		function setPositions() {
+			
+		}
+		
+	}
 
 </script>
 
 <main>
 
-<table ng-controller="MainCtrl">
+<div ng-controller="MainCtrl" id="moduleHolder">
+	
+	<div>
+		<button ng-click="setPositions()">Pos</button>
+	</div>
+
+<table ng-repeat="(key, tip) in campaignTips | orderBy:'key.sort'" class="modules" draggable  >
+	
 	<tr >
-		<th ng-repeat="tip in yesNoPaths"> {{tip.__comments}} </th>
+		<th> <span class="button" edit-name="tip.__comments" key="key"></span>
+		</th>
 	</tr>
 	<tr >
-		<td ng-repeat="tip in yesNoPaths">
-		<div ng-repeat="(key, code) in tip" ng-hide="$first">
-			<strong>{{ key }}</strong>:
-			<input type="text" ng-model="code" class="write" />
-			<span class="button" click-to-edit="code" index="{{key}}"></span>
-		</div><span class="mega-octicon octicon-plus med-icon button addButton" add-new-item="tip"></span></td>
-	</tr>
-</table>
-<div>
-	<button onclick="saveFile(routing)">
-		Save
-	</button>
-</div>
-
-<div ng-controller="MainCtrl">
-	<code>
-		{{ yesNoPaths | json}}
-	</code>
-</div>
-
-<table ng-controller="MainCtrl">
-	<tr >
-		<th ng-repeat="tip in startCampaignTransitions"> {{tip.__comments}} </th>
-	</tr>
-	<tr >
-		<td ng-repeat="tip in startCampaignTransitions">
-		<div ng-repeat="(key, code) in tip" ng-hide="$first">
-			<strong>{{ key }}</strong>:
-			<input type="text" ng-model="code" class="write" />
-			<span class="button" click-to-edit="code" index="{{key}}"></span>
-		</div><span class="mega-octicon octicon-plus med-icon button addButton" add-new-item="tip"></span></td>
-	</tr>
-</table>
-
-<div>
-	<button onclick="saveFile(routing)">
-		Save
-	</button>
-</div>
-
-<div ng-controller="MainCtrl">
-	<code>
-		{{ startCampaignTransitions | json}}
-	</code>
-</div>
-
-<div ng-controller="MainCtrl">
-
-<table>
-
-	<tr >
-		<th ng-repeat="tip in campaignTips"> {{tip.__comments}} </th>
-	</tr>
-	<tr >
-		<td ng-repeat="tip in campaignTips">
+		<td>
 		<div ng-repeat="key in notSorted(tip.optins)" ng-init="code = tip.optins[key]">
 
 			<input class="write" ng-model="code" />
 			<span class="button" click-to-edit="code" index="{{$index}}"></span>
-		</div><span class="mega-octicon octicon-plus med-icon button addButton fade" add-new-item="tip"></span></td>
+		</div><span class="mega-octicon octicon-plus med-icon button addButton fade" add-new-item="tip"></span>
+		<br />
+		<div class="autoMargin">
+			<!-- <button onclick="saveFile(config)" class="hide">Update Database</button> -->
+			<span update-db="update"></span>
+			
+		</div>
+		</td>
 	</tr>
 </table>
 
-<div>
+<div style="clear: both;">
+	<button add-new-module="Hi">Hi</button>
+</div>
+
+<div style="clear: both;">
 	<button onclick="saveFile(config)">
 		Save
 	</button>
@@ -238,6 +406,57 @@
 	</code>
 
 </div>
+
+<table ng-controller="MainCtrl">
+	<tr >
+		<th ng-repeat="tip in yesNoPaths"> {{tip.__comments}} </th>
+	</tr>
+	<tr >
+		<td ng-repeat="tip in yesNoPaths">
+		<div ng-repeat="(key, code) in tip" ng-hide="$first">
+			<strong>{{ key }}</strong>:
+			<input type="text" ng-model="code" class="write" />
+			<span class="button" click-to-edit="code" key="key" index="{{key}}"></span>
+		</div><span class="mega-octicon octicon-plus med-icon button addButton" add-new-item="tip"></span>
+		
+		</td>
+	</tr>
+</table>
+<div>
+	<button onclick="saveFile(routing)">
+		Save
+	</button>
+</div>
+
+
+<table ng-controller="MainCtrl">
+	<tr >
+		<th ng-repeat="tip in startCampaignTransitions"> {{tip.__comments}} </th>
+	</tr>
+	<tr >
+		<td ng-repeat="tip in startCampaignTransitions">
+		<div ng-repeat="(key, code) in tip" ng-hide="$first">
+			<strong>{{ key }}</strong>:
+			<input type="text" ng-model="code" class="write" />
+			<span class="button" click-to-edit="code" key="key" index="{{key}}"></span>
+		</div><span class="mega-octicon octicon-plus med-icon button addButton" add-new-item="tip"></span></td>
+	</tr>
+</table>
+
+<div>
+	<button onclick="saveFile(routing)">
+		Save
+	</button>
+</div>
+
+
+<div ng-controller="MainCtrl"> 
+	<div ng-repeat="person in test | filter:orderByUnicornHeight:'sort':false">
+		{{ person }}
+	</div>
+</div>
+
+
 
 </main>
 
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -1,90 +1,96 @@
 <!DOCTYPE >
 <html>
 	<head>
-		<title></title>	
-		
+		<title></title>
+
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
 		<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.19/angular.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-animate.js"></script>
 		<!-- <script src="//ajax.aspnetcdn.com/ajax/knockout/knockout-3.0.0.js"></script> -->
-		
+
 		<link rel="stylesheet" href="octicons/octicons.css" />
+		<link rel="stylesheet" href="includes/neue.css" />
+		<link rel="stylesheet" href="includes/Proxima Nova/ProximaNova-Reg.otf" />
 		<!-- <script src="includes/jquery.js"></script> -->
 		<!-- <script src="includes/angular.min.js"></script> -->
 		<!-- <script src="includes/angular-sanitize.js"></script> -->
-		
-		
-		
-		
-		
-		
+
 		<style>
+			@font-face {
+				font-family: Proxima Nova;
+				src: url('includes/Proxima Nova/ProximaNova-Reg.otf');
+			}
+		
 			html {
 				font-family: 'Proxima Nova', 'Trebuchet MS', sans-serif;
 				color: #222222;
+				
 			}
 			
+
 			table {
-				border: 1px solid black;
+				/*border: 1px solid black;*/
 				max-width: 100%;
-				min-width: 50%;
+				/*min-width: 50%;*/
 				border-collapse: collapse;
 				margin-left: auto;
-				margin-right:auto;
+				margin-right: auto;
 			}
-			
+
 			td {
 				/*text-align:center;*/
 				line-height: 200%;
-				border-right: solid 1px black;
+				padding: 5%;
+				/*border-right: solid 1px black;*/
 			}
-			
+
 			th {
-				
+
 				padding: 2% 1%;
 				border: 1px solid black;
 				background-color: #4e2b63;
-				color:white;
+				color: white;
 			}
-			
+
 			.write {
-				display:none;
+				display: none;
 				width: 100%;
 			}
-			
+
 			.write input {
 				width: 50;
 			}
-			
+
 			.hidden {
 				display: none;
 			}
-			
+
 			.save {
-				
+
 			}
-			
+
 			.save span {
 				padding-left: 15%;
 			}
-			
+
 			.octicon-check, .octicon-remove-close {
 				color: #dddddd;
 			}
-			
+
 			.octicon-check:hover {
-				color : #9EBF6D;
+				color: #9EBF6D;
 			}
 			.octicon-remove-close:hover {
-				color : #B11623;
+				color: #B11623;
 			}
 			.octicon-plus, .octicon-trashcan {
 				color: #dddddd;
-				
 			}
 			.octicon-plus:hover, .octicon-trashcan:hover {
 				color: #333333;
-			}		
-			
+			}
+
 			.fade {
 				-o-transition: .5s;
 				-webkit-transition: .5s;
@@ -92,30 +98,68 @@
 				transition: .5s;
 			}
 			
+			.right {
+				float:right;
+			}
+
 			.med-icon {
 				font: normal normal 28px octicons;
 			}
-			
+
 			.button {
-				cursor:pointer;
+				cursor: pointer;
 			}
 			
-			pre {outline: 1px solid #ccc; padding: 5px; margin: 5px; }
-			.string { color: green; }
-			.number { color: darkorange; }
-			.boolean { color: blue; }
-			.null { color: magenta; }
-			.key { color: red; }
-			
-			.gridStyle {
-			    border: 1px solid rgb(212,212,212);
-			    width: 400px; 
-			    height: 300px
+			.autoMargin {
+				margin-left: auto;
+				margin-right: auto;
+				text-align:center;
 			}
 
+			pre {
+				outline: 1px solid #ccc;
+				padding: 5px;
+				margin: 5px;
+			}
+
+			.modules {
+				float: left;
+				margin: 1%;
+				display: inline;
+				background-color: #f9f9f9;
+				
+				border-bottom: solid 2px #EEE;
+				border-right: solid 1px #EEE;
+				
+			}
 			
+			.modules:active {
+				background-color: #f1f1f1;
+				/*box-shadow: 3px 3px rgba(0,0,0,.4);*/
+				
+			}
+			
+			#moduleHolder {
+				border: 1px solid black;
+			}
+			
+			.hide {
+				display: none;
+			}
+			
+			
+			.ng-hide {
+			  opacity:0;
+			}
+			
+			.size {
+				font-size: 14px;
+				font-weight: normal;
+			}
+			
+
 		</style>
-		
+
 	</head>
 	<body>
 		@yield('content')
