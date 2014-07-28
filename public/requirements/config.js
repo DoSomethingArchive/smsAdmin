148dcec8d433f6e96ae003bd1a1543075494e5aa
DoSomething <dosomething@DoSomethings-MacBook-Pro.local>
2014-07-28 11:29:43 -0400

    Added necessary includes to the repo

---
+++ public/requirements/config.js
@@ -0,0 +1,19 @@
+var config = {}
+
+config.calendar = {};
+
+
+/**
+
+  See the documentation in README_SERVICES.md for details on how to set this up.
+  When you have filled in all the appropriate information, save this file as 'config.js'
+  
+**/
+
+config.calendar.DEVICE_CODE = ''
+config.calendar.CLIENT_ID = ''
+config.calendar.CLIENT_SECRET = ''
+config.calendar.REFRESH_TOKEN = ''
+
+
+module.exports = config;
